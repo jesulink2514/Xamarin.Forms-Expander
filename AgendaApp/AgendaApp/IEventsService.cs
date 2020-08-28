1554a9e8473c615da873f292119ca35f99511f75
jesulink2514 <jesus.angulo@outlook.com>
2020-08-27 22:36:11 -0500

    Removing additional details

---
+++ AgendaApp/AgendaApp/IEventsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace AgendaApp
+{
+    public interface IEventsService
+    {
+        [Get("/sesiones")]
+        Task<Session[]> GetSessions();
+    }
+
+    public class Session
+    {
+        public int idSesion { get; set; }
+        public string nombreSesion { get; set; }
+        public DateTime fecha { get; set; }
+        public string color { get; set; }
+        public Asistente[] asistentes { get; set; }
+    }
+
+    public class Asistente
+    {
+        public int idAsistente { get; set; }
+        public string nombre { get; set; }
+        public DateTime fechaAsistencia { get; set; }
+    }
+
+}
